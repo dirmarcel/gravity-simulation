9eb133f028441ef39dbb6eacbb4c0347aa31aaa5
dirmarcel <marcepan.lawa576@gmial.com>
2022-03-23 21:55:09 +0100

    23.03.2022 update

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,21 +1,53 @@
- const canvas = document.querySelector('canvas');
- const c = canvas.getContext('2d');
- 
- canvas.width = innerWidth;
- canvas.height = innerHeight;
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
- const mouse = {
-     x: innerWidth / 2,
-     y: innerHeight / 2
- }
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
- addEventListener('mousemove', (e) => {
-     mouse.x = e.clientX
-     mouse.y = e.clientY
- })
+const mouse = {
+  x: innerWidth / 2,
+  y: innerHeight / 2,
+};
 
- addEventListener('resize', () => {
-    canvas.width = innerWidth
-    canvas.height = innerHeight
-  
-  })
+addEventListener("mousemove", (e) => {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+});
+
+addEventListener("resize", () => {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+});
+
+class Ball {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
+  }
+  update() {
+    this.draw();
+  }
+}
+var ball;
+function init() {
+  ball = new Ball(canvas.width / 2, canvas.height / 2, 30, "red");
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+
+  ball.update();
+}
+
+init();
+animate();
